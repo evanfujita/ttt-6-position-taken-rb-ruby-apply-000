977ebfc449dadcd95bfdaf693a3802709a11e397
magical-tutorial-4913 <evanfujita@gmail.com>
2020-10-15 20:26:05 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
- puts "false" if board[index] ==  (""||" "||"  "||nil)
-
+  if board[index] ==  ( "" || " " || "  " || nil)
+    puts "false"  
   elsif board[index] == ("X" || "O")
    puts "true"
 end
